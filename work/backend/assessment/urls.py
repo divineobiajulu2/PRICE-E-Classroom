b284908a7c2ab00c43187c2fc6551b0e6795678b
divineobiajulu <divineobiajulu@gmail.com>
2026-07-17 15:26:41 +0100

    Fix debug_toolbar URL loading to match DEBUG setting for production deploy

--- work/backend/assessment/urls.py
+++ work/backend/assessment/urls.py
@@ -18,16 +18,17 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-import debug_toolbar
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('core.urls')),
-    path('__debug__/', include(debug_toolbar.urls)),
     path("api/assessments/", include("assessments.urls")),
     path("api/courses/assessments/", include("assessments.urls")),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [path('__debug__/', include(debug_toolbar.urls))]
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
